0bf10fc895f8eb70ea947eb683e22cd5f05b2189
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-07 16:30:13 +0700

    membuat alert dengan AlertController

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {NavController, NavParams} from "ionic-angular";
+import {AlertController, NavController, NavParams} from "ionic-angular";
 import {Quote} from "../../data/quoute.interface";
 
 @Component({
@@ -10,7 +10,9 @@ export class QuotesPage implements OnInit {
 
   quote: { category: String, quotes: Quote[], icon: String };
 
-  constructor(private navCtrl: NavController, private navParams: NavParams) {
+  constructor(private navCtrl: NavController,
+              private navParams: NavParams,
+              private alertCtrl: AlertController) {
   }
 
   ngOnInit(): void {
@@ -18,6 +20,25 @@ export class QuotesPage implements OnInit {
   }
 
   onAddToFavorite(value: Quote) {
-
+    let alertMessage = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('OK');
+          }
+        }, {
+          text: 'No, I changed my mind!',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled!')
+          }
+        }
+      ]
+    });
+    alertMessage.present();
   }
 }
