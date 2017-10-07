744a17bd69f2c99651b4981e38b4636f2133d04a
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-07 10:38:24 +0700

    membuat halaman favorites, quotes, quote, library dan settings

---
+++ src/pages/library/library.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {IonicPageModule} from 'ionic-angular';
+import {LibraryPage} from './library';
+
+@NgModule({
+  declarations: [
+    LibraryPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LibraryPage),
+  ],
+})
+export class LibraryPageModule {
+}
